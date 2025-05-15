8b9ac9c0a014f217e3c8fc9529efdccce625bb7b
Himanshu Raj <hraj808404@gmail.com>
2025-05-15 23:28:41 +0530

    Friend card created in Homepage.jsx page

---
+++ client/src/components/NoFriendsFound.jsx
@@ -0,0 +1,10 @@
+const NoFriendsFound = () => {
+  return (
+    <div className="card bg-base-200 p-6 text-center">
+        <h3 className="font-semibold text-lg mb-2">No friends yet...</h3>
+        <p>Connect with new friends below.</p>
+    </div>
+  )
+}
+
+export default NoFriendsFound
--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -25,4 +25,24 @@ export const getAuthUser=async ()=>{
 export const completeOnboarding=async (userData)=>{
 const response=await axiosInstance.post("/auth/onboarding",userData);
 return response.data;
+};
+
+export async function getUserFriends(){
+      const response=await axiosInstance.get("/users/friends");
+      return response.data;
+};
+
+export async function getRecommendedUsers(){
+      const response=await axiosInstance.get("/users");
+      return response.data;
+};
+
+export async function getOutgoingFriendReqs(){
+      const response=await axiosInstance.get("/users/outgoing-friend-reqs");
+      return response.data;
+};
+
+export async function sendFriendRequest(userId) {
+      const response=await axiosInstance.post(`/users/friend-request/${userId}`);
+      return response.data;
 }
