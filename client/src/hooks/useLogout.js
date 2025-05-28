0d13341aef239d24106d12f2b1de077e9e1de5c9
Himanshu Raj <hraj808404@gmail.com>
2025-05-29 00:24:27 +0530

    Logout toast added

--- client/src/hooks/useLogout.js
+++ client/src/hooks/useLogout.js
@@ -1,5 +1,6 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { logout } from "../lib/api.js";
+import toast from "react-hot-toast";
 
 const useLogout = () => {
 
@@ -7,7 +8,9 @@ const useLogout = () => {
 const queryClient=useQueryClient();
   const {mutate,isPending,error}=useMutation({
     mutationFn:logout,
-    onSuccess:()=>queryClient.invalidateQueries({queryKey:["authUser"]})
+    onSuccess:()=>{queryClient.invalidateQueries({queryKey:["authUser"]});
+    toast.success("Logout Successful");
+  }
 
   })
   return {logoutMutation:mutate,isPending,error};
