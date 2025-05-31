d530f0b12382c2b8cc5cee212ae1045548672dda
Himanshu Raj <hraj808404@gmail.com>
2025-06-01 01:45:05 +0530

    bug fixes

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL=import.meta.env.MODE==="development"?"http://localhost:5001/api":"/api"
+const BASE_URL=import.meta.env.VITE_BACKEND_URL
 export const axiosInstance=axios.create({
     baseURL:BASE_URL,
     withCredentials:true, //send cookies with the request
