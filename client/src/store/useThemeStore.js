bd57c0739db268b3092247f3b2d3ca7b21eb6c25
Himanshu Raj <hraj808404@gmail.com>
2025-06-01 01:13:57 +0530

    default theme change

--- client/src/store/useThemeStore.js
+++ client/src/store/useThemeStore.js
@@ -3,7 +3,7 @@ import { create } from 'zustand'
 export const useThemeStore = create((set) => ({
     theme:localStorage.getItem("vibeCall-theme")||"dark",
     setTheme:(theme)=>{
-        localStorage.setItem("vibeCall-theme",theme);
+        localStorage.setItem("vibeCall-theme",lemonade);
         set({theme});
     }
 }))
