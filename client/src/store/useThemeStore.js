0ea2f72f08cdf9e814c63942f624653a17165b15
Himanshu Raj <hraj808404@gmail.com>
2025-06-01 12:50:49 +0530

    theme update

--- client/src/store/useThemeStore.js
+++ client/src/store/useThemeStore.js
@@ -3,7 +3,7 @@ import { create } from 'zustand'
 export const useThemeStore = create((set) => ({
     theme:localStorage.getItem("vibeCall-theme")||"lemonade",
     setTheme:(theme)=>{
-        localStorage.setItem("vibeCall-theme",lemonade);
+        localStorage.setItem("vibeCall-theme",theme);
         set({theme});
     }
 }))
